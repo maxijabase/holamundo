580a848e9b85090bde3f4142ec9f36971bd916e1
Maximiliano Jabase <maxijabase@gmail.com>
2025-04-18 01:19:46 -0300

    creo mi primer archivo

---
+++ index.ts
@@ -0,0 +1,9 @@
+let nombre: string = "maxi"
+let edad: number = 24;
+let pais: string = "Argentina"
+
+function PrintMyInfo(): void {
+  console.log(`Hola! mi nombre es ${nombre}, tengo ${edad} años, y vivo en ${pais}`)
+}
+
+PrintMyInfo();
